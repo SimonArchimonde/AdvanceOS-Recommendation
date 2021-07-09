8b5c86a3807f8a0c15c05a422fc988dffdc970c1
Ignoramus <Ignoramus0817@gmail.com>
2021-07-09 15:05:24 +0800

    deleted redundant comment

--- src/main/scala/AR/Main.scala
+++ src/main/scala/AR/Main.scala
@@ -7,11 +7,14 @@ import AR.main.RecPartUserRdd
 
 object Main {
   def main(args: Array[String]): Unit = {
+    // start time
     val start = System.currentTimeMillis()
+
     assert(
       args.length >= 2,
       "Usage: JavaFPGrowthExample <input-file> <output-file> <tmp-file> <spark.cores.max (optional)> <spark.executor.cores (optional)>"
     )
+    
     val otherArgs = for (i <- 0 until args.length if i >= 2) yield args(i)
     val myConf = Conf.getConfWithoutInputAndOutput(otherArgs.toArray)
     println("args:" + myConf.toString())
@@ -21,6 +24,8 @@ object Main {
     myConf.outputFilePath = args(1)
     myConf.tempFilePath = args(2)
     FP_Growth.total(myConf, conf)
+
+    // end time
     val end = System.currentTimeMillis()
     println("total time: " + (end - start) / 1000 + "s")
 //
